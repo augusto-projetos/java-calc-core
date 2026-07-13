941526be4cfb793cfe0ecc5b63e3c665f7ec0e3f
Luiz Freitas <luiz.alfreitas12@gmail.com>
2026-07-13 16:47:55 -0300

    feat: adiciona motor de cálculo básico e robô de testes (#2)

---
+++ src/main/java/com/calcJava/calculator/Calculator.java
@@ -0,0 +1,27 @@
+package com.calcJava.calculator;
+
+public class Calculator {
+    
+    public double calcular(double num1, double num2, String operacao) {
+
+        switch (operacao) {
+            case "+":
+                return num1 + num2;
+
+            case "-":
+                return num1 - num2;
+
+            case "*":
+                return num1 * num2;
+
+            case "/":
+                if (num2 == 0) {
+                    throw new ArithmeticException("Não é possível dividir por zero.");
+                }
+                return num1 / num2;
+
+            default:
+                throw new IllegalArgumentException("Operação inválida: " + operacao);
+        }
+    }
+}
---
+++ src/test/java/com/calcJava/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.calcJava.calculator;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+        "5,   5,  '+',  10.0",
+        "10,  4,  '-',  6.0",
+        "3,   4,  '*',  12.0",
+        "10,  2,  '/',  5.0",
+        "2.5, 2,  '+',  4.5"
+    })
+    public void deveCalcularOperacoesBasicasCorretamente(double num1, double num2, String operacao, double resultadoEsperado) {
+        Calculator calc = new Calculator();
+        
+        double resultadoObtido = calc.calcular(num1, num2, operacao);
+        
+        assertEquals(resultadoEsperado, resultadoObtido, 
+            () -> String.format("Erro ao calcular: %s %s %s", num1, operacao, num2));
+    }
+}
