79f6d01690bf7af3bfa42bcb8b384fc0a13d45a5
Luiz Freitas <luiz.alfreitas12@gmail.com>
2026-07-20 20:01:33 -0300

    feat: gera pasta para executar projeto (#8)

--- src/main/java/com/calcJava/calculator/Main.java
+++ src/main/java/com/calcJava/calculator/Main.java
@@ -10,3 +10,11 @@ public class Main {
         });
     }
 }
+
+/*
+Comando para gerar executável -> .\mvnw clean package
+
+Execute o comando -> & "C:\Program Files\Java\jdk-17\bin\jpackage.exe" --type app-image --name Calculadora --input target --main-jar java-calc-core-1.0-SNAPSHOT.jar --main-class com.calcJava.calculator.Main --dest dist --icon src\main\resources\ico\calculator.ico
+
+Empacote no zip a pasta dist/Calculadora para compartilhar o projeto
+*/
